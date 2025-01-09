ece2f0432b35947f78bff29f5d2f2d3efc21d14b
dsyengo <deniswilson028@gmail.com>
2025-01-09 10:46:33 +0300

    deriv login 3

--- dashboard.js
+++ dashboard.js
@@ -173,7 +173,7 @@ logoutBtn.addEventListener('click', () => {
 
 // Deriv OAuth configuration
 const clientId = app_id; // Replace with your OAuth Client ID
-const redirectUri = 'https://app.deriv.com/dtrader'; // Replace with your redirect URI
+const redirectUri = 'https://deriv-app.netlify.app/dashboard.html'; // Replace with your redirect URI
 
 // Trade button event listener
 tradeBtn.addEventListener('click', () => {
